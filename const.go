b8d6f64f19cc6524664d26492f112119da9796f4
Viktor Khodorchenko <vkhodorchenko@openprovider.nl>
2019-07-01 19:13:39 +0700

    IsValidBorgRepo, NewBorgRepo

--- const.go
+++ const.go
@@ -3,18 +3,20 @@ package main
 const LogPrefix = "BorGoZ"
 
 const (
-	EnvVariableHost  = "BORGOZ_HOST"
-	EnvVariablePort  = "BORGOZ_PORT"
+	EnvVariableHost           = "BORGOZ_HOST"
+	EnvVariablePort           = "BORGOZ_PORT"
 	EnvVariableReposDirectory = "BORGOZ_REPOS_DIR"
-	EnvVariableDefaultRepoKey  = "BORGOZ_DEFAULT_REPO_KEY"
-	EnvVariableLogLevel = "BORGOZ_LOG_LEVEL"
+	EnvVariableDefaultRepoKey = "BORGOZ_DEFAULT_REPO_KEY"
+	EnvVariableLogLevel       = "BORGOZ_LOG_LEVEL"
+	EnvVariableBorgBin        = "BORGOZ_BORG_BIN"
 )
 
 const (
-	DefaultHost = "127.0.0.1"
-	DefaultPort = "8080"
+	DefaultHost           = "127.0.0.1"
+	DefaultPort           = "8080"
 	DefaultReposDirectory = "./"
-	DefaultLogLevel = "INFO"
+	DefaultLogLevel       = "INFO"
+	DefaultBorgBin        = "borg"
 )
 
 const ErrorExitNewApplication int = 1 // Exit when Fatal error during NewApplication
